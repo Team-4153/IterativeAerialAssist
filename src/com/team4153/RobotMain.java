1658aa72ec1655dd71e0733f668a1c9ad1214140
Developer <Developer@64.106.49.122>
2014-02-16 14:15:30 -0700

    Autonomous looks for target from beginning

--- src/com/team4153/RobotMain.java
+++ src/com/team4153/RobotMain.java
@@ -31,10 +31,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class RobotMain extends IterativeRobot {
 
-    /**
-     * The ideal angle to place the arm at to fire at the fire distance
-     */
-    public static double SHOOTING_ANGLE = 2.5;
+    
 
     JoystickHandler joystick;
     Chassis chassis;
@@ -47,25 +44,9 @@ public class RobotMain extends IterativeRobot {
     Winch winch;
     DistanceAngleTable angleTable;
     ImageStorer storer;
+    Autonomous autonomous;
 
-    private static long autoStartTime = -1;
-    boolean autoHot = false;
-    boolean autoTarget = false;
-    boolean autoShot = false;
-
-    private final double FIRE_DISTANCE = 120;
-    private final double MAX_AUTONOMOUS_SPEED = 200;
-
-    public static final int OVERSHOOT_CORRECTION = 6;
-    public static final int ULTRASONIC_DISPLACEMENT = 5;
-    public static final double AUTONOMOUS_SLOWDOWN_AMOUNT = 0.4;
-    public static final double AUTONOMOUS_SLOWDOWN_PERCENT = 1.35;
-    public static final int EXCEPTION_FIRE_TIME = 9000;
-    public static final int EXCEPTION_STOP_TIME = 5000;
-
-    int counter = 0;
-    boolean withinFiringDistance = false;
-    boolean withinSlowdownDistance = false;
+    
 
     /**
      * This function is run when the robot is first started up and should be
@@ -83,6 +64,7 @@ public class RobotMain extends IterativeRobot {
         angleTable = new DistanceAngleTable(arm);
         vision = new Vision();
         storer = new ImageStorer(vision.getCamera());
+        autonomous=new Autonomous(chassis,shooter,arm, flippers);
         //storer.start();
         startCompressor();
         Sensors.getGyro().getAngle();
@@ -95,81 +77,14 @@ public class RobotMain extends IterativeRobot {
      */
     public void autonomousPeriodic() {
         dashboardComm.execute();
-        try {
-            System.out.println("Arbitrary Drive value: " + chassis.rightFront.getX());
-        } catch (CANTimeoutException ex) {
-            ex.printStackTrace();
-        }
-
-        //note this line both performs the lookup and moves the arm.
-        angleTable.execute(-1);
-
-        double distance = Sensors.getFilteredUltrasonicDistance();
-        final double fireDistance = FIRE_DISTANCE + ULTRASONIC_DISPLACEMENT + OVERSHOOT_CORRECTION;
+        boolean as1=Sensors.getAutoSwitch1().get();
+        boolean as2=Sensors.getAutoSwitch2().get();
         
-        // if the robot is already withing the firing distance we do not want to
-        // run any of the movement code - this 'if' makes sure of that
-        if (!withinFiringDistance) {
-            System.out.println("distance "+distance);
-            // if the robot has run longer than the exception stop time it sets up
-            // as if it were within range - there is probably something wrong with
-            // the ultrasonic
-            if (distance >= fireDistance && getMatchTime() < EXCEPTION_STOP_TIME) {
-                if (distance >= fireDistance * AUTONOMOUS_SLOWDOWN_PERCENT && !withinSlowdownDistance) {
-                    chassis.driveForward(MAX_AUTONOMOUS_SPEED);
-                    System.out.println("Full Speed: " + distance);
-                } else {
-                    chassis.driveForward(MAX_AUTONOMOUS_SPEED * AUTONOMOUS_SLOWDOWN_AMOUNT);
-                    System.out.println("Slow Down Speed: " + distance);
-                    withinSlowdownDistance = true;
-                }
-                //SmartDashboard.putNumber("Counter", counter++);
-//                SmartDashboard.putNumber("Ultrasonic Running", distance);
-                counter = 0;
-            } else {
-                withinFiringDistance = true;
-                System.out.println("Stopped: " + distance);
-                chassis.driveHalt();
-                if (counter++ == 0) {
-                    SmartDashboard.putNumber("Ultrasonic Stop", distance);
-                }
-                //SmartDashboard.putNumber("Counter", counter);
-            }
+        if(as1&&!as2){
+            autonomous.shootHighGoal();
         }
-
-        /*if (withinFiringDistance) {
-         chassis.turn(90);
-         }*/
-        
-        if(!autoTarget){
-            vision.execute(-1);
-            if(vision.isTarget()){
-                autoTarget=true;
-                autoHot=vision.isHot();
-                System.out.println("Target found, Hot: "+autoHot);
-            }
-        }
-        
-        // this will run when the arm is in position and we are in range
-        if (withinFiringDistance && Math.abs(SHOOTING_ANGLE - arm.getDesiredAngle()) < RobotConstants.ARM_TOLERANCE) {
-            /*vision.execute(-1);
-            SmartDashboard.putBoolean("Target: ", vision.isTarget());
-            SmartDashboard.putBoolean("Hot: ", vision.isHot());
-            if (vision.isTarget() && vision.isHot() && !autoShot) {
-                shooter.execute(-1);
-                autoShot = true;
-            }*/
-            
-            if(!autoShot&&autoTarget&&(autoHot&&getMatchTime()<5000||!autoHot)){
-                shooter.execute(-1);
-                autoShot = true;
-            }
-                
-        }
-
-        if (getMatchTime() > EXCEPTION_FIRE_TIME && !autoShot) {
-            shooter.execute(-1);
-            autoShot = true;
+        if(!as1&&as2){
+            autonomous.dropInLowGoal();
         }
     }
 
@@ -178,28 +93,14 @@ public class RobotMain extends IterativeRobot {
      * shooting angle.
      */
     public void autonomousInit() {
-        resetAuto();
-        autoStartTime = System.currentTimeMillis();
-        angleTable.execute(-1);
+        
         dashboardComm.execute();
     }
 
     /**
      * Resets variables used in autonomous.
      */
-    public void resetAuto() {
-        autoStartTime = -1;
-        autoHot = false;
-        autoTarget = false;
-        autoShot = false;
-        withinFiringDistance = false;
-        counter = 0;
-        dashboardComm.execute();
-    }
     
-    public static long getMatchTime (){
-        return System.currentTimeMillis() - autoStartTime;
-    }
 
     /**
      * This function is called periodically during operator control.
